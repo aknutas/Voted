d809ef537ea98765cf97485904b58ae3a9803a6f
aknutas <antti.knutas@iki.fi>
2013-09-05 11:47:06 +0300

    Added clarifications to QR Code JS, added styling around QR codes.

--- public/javascripts/poll_qr_delegate.js
+++ public/javascripts/poll_qr_delegate.js
@@ -1,4 +1,7 @@
 $( document ).delegate("#pollshow", "pageinit", function() {
     var pid = $('#hdata').attr('pid');
-    $('#qrcode').qrcode('http://localhost:3000/polls/' + pid);
+    $('#qrcode').qrcode({
+        width: "200",
+        height: "200",
+        text: 'http://localhost:3000/polls/' + pid});
 });
